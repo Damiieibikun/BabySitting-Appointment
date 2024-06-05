a8ac4abf740d59d6040a0b6c757a558d4129ee50
Damilola Ibikunle <damiieibikun@gmail.com>
2024-06-05 21:50:39 +0100

    added cost breakdown info to appointment details

--- js/appointment.js
+++ js/appointment.js
@@ -1,16 +1,130 @@
-let appointmentDetails = JSON.parse(localStorage.getItem('Appointment-details'))
-let sitterSelected = localStorage.getItem('Sitter')
+let appointmentDetails = JSON.parse(
+  localStorage.getItem("Appointment-details")
+);
+let sitterSelected = localStorage.getItem("Sitter");
 //Client's info
 
-document.getElementById('guardian-name').innerText = appointmentDetails.guardianName
-document.getElementById('guardian-number').innerText = appointmentDetails.phoneNumber
-document.getElementById('guardian-email').innerText = appointmentDetails.email
-document.getElementById('child-name').innerText = appointmentDetails.childName
-document.getElementById('child-age').innerText = `${appointmentDetails.childAge} years`
+document.getElementById("guardian-name").innerText =
+  appointmentDetails.guardianName;
+document.getElementById("guardian-number").innerText =
+  appointmentDetails.phoneNumber;
+document.getElementById("guardian-email").innerText = appointmentDetails.email;
+document.getElementById("child-name").innerText = appointmentDetails.childName;
+document.getElementById(
+  "child-age"
+).innerText = `${appointmentDetails.childAge} years`;
 
 //Appointment info
-document.getElementById('service-selected').innerText = appointmentDetails.serviceType
-document.getElementById('sitter-selected').innerText = sitterSelected
-document.getElementById('date-selected').innerText = appointmentDetails.date
-document.getElementById('time-selected').innerText = appointmentDetails.time
-document.getElementById('duration-selected').innerText = appointmentDetails.duration
+document.getElementById("service-selected").innerText =
+  appointmentDetails.serviceType;
+document.getElementById("sitter-selected").innerText = sitterSelected;
+document.getElementById("date-selected").innerText = appointmentDetails.date;
+document.getElementById("time-selected").innerText = appointmentDetails.time;
+document.getElementById("duration-selected").innerText =
+  appointmentDetails.duration;
+
+// cost breakdown
+function serviceCharge() {
+  let rate = null;
+  if (appointmentDetails.serviceType === "Day") {
+    rate = 5 * 8;
+    document.getElementById(
+      "cost-serviceinfo"
+    ).innerHTML = `<b>${appointmentDetails.serviceType}(8am - 4pm)</b>: Base Rate ($5 per hour): <b>$${rate}</b>`;
+  } else if (appointmentDetails.serviceType === "Evening") {
+    rate = 5 * 1.1 * 6;
+    document.getElementById(
+      "cost-serviceinfo"
+    ).innerHTML = `<b>${appointmentDetails.serviceType}(4pm - 10pm)</b>: 1.1 X Base Rate ($5 per hour): <b>$${rate}</b>`;
+  } else {
+    rate = 5 * 1.5 * 10;
+    document.getElementById(
+      "cost-serviceinfo"
+    ).innerHTML = `<b>${appointmentDetails.serviceType}(10pm - 8am)</b>: 1.5 X Base Rate ($5 per hour): <b>$${rate}</b>`;
+  }
+
+  return rate;
+}
+
+function childRate() {
+  let rate = serviceCharge();
+  let childAge = parseInt(appointmentDetails.childAge);
+  if (childAge === 0 || childAge === 1) {
+    rate *= 1.2;
+    document.getElementById(
+      "cost-childinfo"
+    ).innerHTML = `<b> Newborn - 1 year</b>: 1.2x Service Rate: <b>$${rate}</b>`;
+  } else if (childAge > 1 && childAge < 6) {
+    rate *= 1.1;
+    document.getElementById(
+      "cost-childinfo"
+    ).innerHTML = `<b> 1 year - 5 years</b>: 1.1x Service Rate: <b>$${rate}</b>`;
+  } else {
+    rate = rate;
+    document.getElementById(
+      "cost-childinfo"
+    ).innerHTML = `<b> Over 5 years</b>: 1x Service Rate: <b>$${rate}</b>`;
+  }
+
+  return rate;
+}
+
+
+function durationRate(){
+    let rate = childRate()
+    if(appointmentDetails.duration === '1 week'){
+        rate *= 7
+        document.getElementById(
+            "cost-durationinfo"
+          ).innerHTML = `<b> ${appointmentDetails.duration}</b>: <b>$${rate}</b>`;
+    }
+    else if(appointmentDetails.duration === '2 weeks'){
+        rate*=14
+        document.getElementById(
+            "cost-durationinfo"
+          ).innerHTML = `<b> ${appointmentDetails.duration}</b>: <b>$${rate}</b>`;
+    }
+    else if(appointmentDetails.duration === '3 weeks'){
+        rate*=21
+        document.getElementById(
+            "cost-durationinfo"
+          ).innerHTML = `<b> ${appointmentDetails.duration}</b>: <b>$${rate}</b>`;
+    }
+    else if(appointmentDetails.duration === '1 month'){
+        rate*=30
+        document.getElementById(
+            "cost-durationinfo"
+          ).innerHTML = `<b> ${appointmentDetails.duration}</b>: <b>$${rate}</b>`;
+    }
+
+    return rate
+
+}
+
+// console.log(durationRate());
+
+function urgencyFee(){
+    let rate = durationRate()
+   
+    let differenceMilisecond = new Date(appointmentDetails.date) - new Date();
+    let millisecondsInADay = 1000 * 60 * 60 * 24;
+    let dayDifference = Math.floor(differenceMilisecond / millisecondsInADay);
+    if (dayDifference < 7){
+        rate*=1.2
+        
+        document.getElementById(
+            "urgency"
+          ).innerHTML = `<b> Urgency Fee: </b>Booking Date within 7 days <b>$${(rate/1.2) *0.2}</b>`;
+
+    }
+    else{
+        rate = rate
+    }
+    return rate
+}
+
+document.getElementById(
+    "total-cost"
+  ).innerHTML = `<b>$${urgencyFee()}</b>`
+
+
