e93b12bcf723851fde441cf3ad441740c40efdbc
Damilola Ibikunle <damiieibikun@gmail.com>
2024-06-05 20:22:12 +0100

    added client and booking info to booking page with styles

--- js/appointment.js
+++ js/appointment.js
@@ -1,4 +1,16 @@
 let appointmentDetails = JSON.parse(localStorage.getItem('Appointment-details'))
 let sitterSelected = localStorage.getItem('Sitter')
-document.getElementById('service-selected').innerText = `${appointmentDetails.serviceType} sitters`
+//Client's info
+
+document.getElementById('guardian-name').innerText = appointmentDetails.guardianName
+document.getElementById('guardian-number').innerText = appointmentDetails.phoneNumber
+document.getElementById('guardian-email').innerText = appointmentDetails.email
+document.getElementById('child-name').innerText = appointmentDetails.childName
+document.getElementById('child-age').innerText = `${appointmentDetails.childAge} years`
+
+//Appointment info
+document.getElementById('service-selected').innerText = appointmentDetails.serviceType
 document.getElementById('sitter-selected').innerText = sitterSelected
+document.getElementById('date-selected').innerText = appointmentDetails.date
+document.getElementById('time-selected').innerText = appointmentDetails.time
+document.getElementById('duration-selected').innerText = appointmentDetails.duration
